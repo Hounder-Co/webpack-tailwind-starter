dda51e8854beb1a974cc34da8dce86925be9e67a
Justin Hough <justinrayhough@gmail.com>
2021-10-15 17:21:43 -0600

    added tailwind config for project customization

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  dark: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
